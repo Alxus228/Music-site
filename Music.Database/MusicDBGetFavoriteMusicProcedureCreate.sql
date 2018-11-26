8ba5fbffca578dfd2ca04285e81fea0e95ef2956
Alxus228 <40440883+Alxus228@users.noreply.github.com>
2018-11-26 21:54:27 +0300

    Rename MusicDBGetFavoriteMusicProcedureCreate.sql to Music.Database/MusicDBGetFavoriteMusicProcedureCreate.sql

---
+++ Music.Database/MusicDBGetFavoriteMusicProcedureCreate.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetFavoriteMusic]
+@UserId INT
+AS
+	SELECT *
+--  SELECT [Id], [Name], [Author], [GenreId], [Year], [Lyrics], [SizeInMB], [DurationInSeconds], [Quality], [Icon], [AudioFile], [DateOfCreation], [UserIdWhoCreated]
+	FROM [dbo].[Track] AS t
+	LEFT JOIN [dbo].[Track_User] AS tu
+	ON tu.TrackId = t.Id
+	WHERE tu.UserId = @UserId
