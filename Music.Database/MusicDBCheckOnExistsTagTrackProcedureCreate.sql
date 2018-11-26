32fcd163c5ed8178f2b7b19e6ee7929cf3e28cd8
Alxus228 <40440883+Alxus228@users.noreply.github.com>
2018-11-26 21:53:07 +0300

    Rename MusicDBCheckOnExistsTagTrackProcedureCreate.sql to Music.Database/MusicDBCheckOnExistsTagTrackProcedureCreate.sql

---
+++ Music.Database/MusicDBCheckOnExistsTagTrackProcedureCreate.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[CheckOnExistsTagTrack]
+@TrackId INT, @TagId INT
+AS
+IF (EXISTS (SELECT * FROM [dbo].Tag_Track WHERE TrackId = @TrackId AND TagId = @TagId))
+BEGIN SELECT 1 END
+ELSE
+BEGIN SELECT 0 END
