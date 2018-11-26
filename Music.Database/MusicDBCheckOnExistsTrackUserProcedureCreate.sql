f554f3a8ed0b38d97bda9a8e6fd0857eea586212
Alxus228 <40440883+Alxus228@users.noreply.github.com>
2018-11-26 21:53:35 +0300

    Rename MusicDBCheckOnExistsTrackUserProcedureCreate.sql to Music.Database/MusicDBCheckOnExistsTrackUserProcedureCreate.sql

---
+++ Music.Database/MusicDBCheckOnExistsTrackUserProcedureCreate.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[CheckOnExistsTrackUser]
+@TrackId INT, @UserId INT
+AS
+IF (EXISTS (SELECT * FROM [dbo].[Track_User] WHERE TrackId = @TrackId AND UserId = @UserId))
+BEGIN SELECT 1 END
+ELSE
+BEGIN SELECT 0 END
