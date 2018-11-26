706195231801d35dd3946ad9e53a7280ccd6c1ed
Alxus228 <40440883+Alxus228@users.noreply.github.com>
2018-11-26 21:55:30 +0300

    Rename MusicDBGetTracksByTagProcedureCreate.sql to Music.Database/MusicDBGetTracksByTagProcedureCreate.sql

---
+++ Music.Database/MusicDBGetTracksByTagProcedureCreate.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetTracksByTag]
+@TagId INT
+AS
+	SELECT *
+--  SELECT [Id], [Name], [Author], [GenreId], [Year], [Lyrics], [SizeInMB], [DurationInSeconds], [Quality], [Icon], [AudioFile], [DateOfCreation], [UserIdWhoCreated]
+	FROM [dbo].[Track] AS t
+	LEFT JOIN [dbo].[Tag_Track] AS tt
+	ON tt.TrackId = t.Id
+	WHERE tt.TagId = @TagId
